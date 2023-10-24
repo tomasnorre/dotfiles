debd73b103b3f463fb874d334748ea04fff46286
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2023-10-24 14:26:26 +0200

    Update FreeDisk cmd

--- files/.zsh/login.zsh
+++ files/.zsh/login.zsh
@@ -1,5 +1,5 @@
 os=`lsb_release -d | awk '{printf "%s %s", $2, $3}'`
-rootFree=`df -h | grep root | awk '{print $4}' | tr -d '\n'`
+rootFree=`df -h | grep ubuntu | awk '{print $4}' | tr -d '\n'`
 memAvailable=`free -m | grep "Mem" | awk '{printf "%.1fG", $7/1024}'`
 loadAvg=`cat /proc/loadavg | awk '{printf "%s %s %s", $1, $2, $3}'`
 user=`whoami`
