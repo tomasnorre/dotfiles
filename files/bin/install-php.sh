4fc51b65c7cc0021e3e5c903dbe4c71cf4e03054
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2025-12-29 16:11:57 +0100

    fix: small changes

--- files/bin/install-php.sh
+++ files/bin/install-php.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-PHP_VERSIONS='8.1 8.2 8.3 8.4'
+#PHP_VERSIONS='8.4'
+PHP_VERSIONS='8.5'
 PHP_EXTS='mongodb cli common curl gmp intl mbstring mysql opcache readline soap sqlite3 xml zip pgsql xdebug'
 #PHP_EXTS='cli common curl intl soap xml zip'
 TOTAL="";
